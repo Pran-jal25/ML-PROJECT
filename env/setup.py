d6dc916c30255d04f880a20c8265233cec46f8c8
Pranjal <pranjalchoubeyxc@gmail.com>
2026-06-09 15:10:47 +0530

    Enhance docstring in get_requirements function
    Added docstring to clarify the purpose of the get_requirements function.

--- env/setup.py
+++ env/setup.py
@@ -6,6 +6,8 @@ def get_requirements(file_path:str)->List[str]:
     '''
     this function will return the list of requirements
     '''
+    '''these are the requirements needed'''
+    
     requirements=[]
     with open(file_path) as file_obj:
         requirements=file_obj.readlines()
